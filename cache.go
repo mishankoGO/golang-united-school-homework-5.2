688f290d3ffd1a299fca5d8985b489705f03faa7
Mikhail <mishajudoist@gmail.com>
2022-05-23 20:09:05 +0300

    implemented all methods v1

--- cache.go
+++ cache.go
@@ -3,21 +3,38 @@ package cache
 import "time"
 
 type Cache struct {
+	vars      map[string]string
+	deadlines map[string]time.Time
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	if val, ok := c.vars[key]; ok {
+		if c.deadlines[key].Sub(time.Now()) <= time.Second*0 {
+			delete(c.deadlines, key)
+			return "", false
+		}
+		return val, ok
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.vars[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var keys []string
+	for k, _ := range c.vars {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.vars[key] = value
+	c.deadlines[key] = deadline
 }
