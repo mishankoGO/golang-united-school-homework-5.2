b14fda9fee9f20c78de66ff4de716d4a8d3e982f
Mikhail <mishajudoist@gmail.com>
2022-05-23 20:17:27 +0300

    added check on deadlines v3

--- cache.go
+++ cache.go
@@ -17,7 +17,7 @@ func NewCache() Cache {
 
 func (c Cache) Get(key string) (string, bool) {
 	if val, ok := c.vars[key]; ok {
-		if c.deadlines[key].Sub(time.Now()) <= time.Second*0 {
+		if _, ok := c.deadlines[key]; c.deadlines[key].Sub(time.Now()) <= time.Second*0 && ok {
 			delete(c.deadlines, key)
 			return "", false
 		}
